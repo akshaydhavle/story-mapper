15ef4169c9d4ecef9402e0a70c17a5f1e4aa369b
Kevin Braun <kevin.d.braun@gmail.com>
2011-10-18 20:21:50 -0500

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StoryMapper::Application.config.secret_token = '8c609191d389e903bab1fb69fe31f7cb57c60ed762f283b3e6006da5e65d6de762efbbece11435f72587348722c4c170e4cd9ae5e186f336624daafc9613b1ff'
