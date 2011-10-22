573f2373e45276e3533d2213bc68092754b832fc
Kevin Braun <kevin.d.braun@gmail.com>
2011-10-22 14:19:40 -0500

    Implement creation/deletion of projects.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,4 +1,5 @@
 class ProjectsController < ApplicationController
+  before_filter :authenticate_user!
   
   # View all projects
   def index
@@ -9,5 +10,28 @@ class ProjectsController < ApplicationController
   # Show an individual project
   def show
     @project = Project.find(params[:id])
+  end
+  
+  # Create a new project
+  def new
+    @project = Project.new
+  end
+  
+  def create
+    @project = Project.new(params[:project])
+    if @project.save
+      redirect_to projects_path, :notice => "Successfully created #{@project.name}"
+    else
+      render "new"
+    end
+  end
+  
+  # remove a project
+  def destroy
+    @project = Project.find(params[:id])
+    name = @project.name
+    @project.destroy
+  
+    redirect_to projects_path, :notice => "Deleted #{name}"
   end
 end
