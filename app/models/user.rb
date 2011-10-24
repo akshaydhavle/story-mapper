79a7800193483a77b4abb786b165720d310b761c
Kevin Braun <kevin.d.braun@gmail.com>
2011-10-23 20:55:37 -0500

    Add username field to User, and allow login with either username or email. Shuffle around some styles into more appropriate locations.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,17 @@ class User < ActiveRecord::Base
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :rememberable, :trackable, :validatable
 
+  # Virtual attribute for authenticating via email or username
+  attr_accessor :login
+
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :username, :email, :login, :password, :password_confirmation, :remember_me
+
+  protected
+    # Overridden to allow for username or email login
+    def self.find_for_database_authentication(warden_conditions)
+      conditions = warden_conditions.dup
+      login = conditions.delete(:login)
+      where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    end
 end
